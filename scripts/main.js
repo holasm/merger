5b00ae6da7fe8eec4f0bd9ad587b3b416b40eaf2
subhasis maity <holasm@gmail.com>
2016-07-06 04:53:51 +0530

    add filtrable list

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,20 @@
 var data = {
-	text: {
-		head: 'I am the poor guy.'
-	}
+	names: [
+		'I am the poor guy.',
+		'subhasis',
+		'maity',
+		'debasis',
+		'mnana',
+		'amar'
+	],
+	fil: ''
 }
 
 Vue.component('input-box', {
-	template: '<input type="text" @keyup="update">',
+	template: '<input type="text" @keyup="update" v-model="filter">',
 	methods: {
 		update: function (e) {
-			data.text.head = e.target.value
+			data.fil = this.filter
 		}
 	}
 })
